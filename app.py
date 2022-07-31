15aa8aab0272b412430a2c4497d76bf2ff322b02
KushKumarK <91371411+KushKumarK@users.noreply.github.com>
2022-07-31 14:30:07 +0530

    2

--- app.py
+++ app.py
@@ -2,14 +2,50 @@ from flask import Flask, request, jsonify
 
 app = Flask(__name__)
 
+action = ""
+actions = ["LIFO",
+           "Irrigation",
+           "Fertilizer",
+           "Plant Seed",
+           "Remote Control"]
+
 
 @app.route('/', methods=['GET', 'POST'])
 def get_data():  # put application's code here
     data = request.get_json(force=True)
-    print(data["action"])
-
+    global action
+    action = data["action"]
+    if action in actions:
+        pass
+    elif action == "":
+        pass
+    else:
+        actions.append(action)
     return "Sending Data to B.A.R.F"
 
+
+@app.route('/action', methods=["GET", 'POST'])
+def send_data():
+    if action == "":
+        return "20"
+    else:
+        rec = str(actions.index(action)+1)
+        return rec
+
+@app.route("/delete_tile", methods=["GET", "POST"])
+def delete_data():
+    delData = request.get_json(force=True)
+    global action
+    if action == delData["action"]:
+        action = ""
+        if (actions.index(delData["action"])<6):
+            return "Permission Denied"
+        else:
+            del(actions[int(delData["index"])])
+            return "Deleted successfully"
+    else:
+        del (actions[int(delData["index"])])
+        return "Deleted successfully"
 
 if __name__ == '__main__':
     app.run(host="0.0.0.0", port=8000, debug=True)
